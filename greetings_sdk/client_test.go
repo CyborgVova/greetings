f7bfb30c74c46d678fb4948021694b1df7b52b52
cyborg-K56CB <eroxin79@mail.ru>
2026-08-29 04:25:00 +0300

    start write client tests

--- greetings_sdk/client_test.go
+++ greetings_sdk/client_test.go
@@ -1,1 +1,13 @@
 package greetings_sdk
+
+import (
+    _ "context"
+    _ "net/http"
+    _ "net/http/httptest"
+    "testing"
+    _ "time"
+)
+
+func TestClient_Greetings(t *testing.T) {}
+
+func TestClient_Timeout(t *testing.T) {}
